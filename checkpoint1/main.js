a914f9d6bd095bafe516624a5be99e586335ffeb
Alfonsus Wiliyanto <alponklepon@gmail.com>
2023-11-10 16:40:19 +0700

    Final Attempt

--- checkpoint1/main.js
+++ checkpoint1/main.js
@@ -1,3 +1,4 @@
+// DarkMode
 let darkTheme = document.body;
 let iconChange = document.getElementById("iconChange");
 function darkMode() {
@@ -6,3 +7,26 @@ function darkMode() {
     ? (iconChange.src = "assets/dark-mode/sun.png")
     : (iconChange.src = "assets/dark-mode/moon.png");
 }
+// Navbar Responsive
+let sidebar = document.querySelector(".hamburger");
+sidebar.onclick = function () {
+  let navSidebar = document.querySelector("nav");
+  let menuSidebar = document.querySelector(".navMenuWrapper");
+  let buttonSidebar = document.querySelector(".navRightWrapper");
+  navSidebar.classList.toggle("active");
+  menuSidebar.classList.toggle("active");
+  buttonSidebar.classList.toggle("active");
+};
+
+// Page Orbserver
+const observer = new IntersectionObserver((area) => {
+  area.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animation");
+    } else {
+      entry.target.classList.remove("animation");
+    }
+  });
+});
+const leftToRight = document.querySelectorAll(".hidden");
+leftToRight.forEach((el) => observer.observe(el));
